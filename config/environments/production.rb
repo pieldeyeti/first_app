d85bba15c006d77f4d8af757224b09b1a23f11bb
Piel de Yeti <pieldeyeti@gmail.com>
2011-12-14 19:11:19 +0100

    Cambiada configuración de producción (heroku)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,9 @@ FirstApp::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  
+  # Incluido porque en heroku no carga las imagenes de assets
+  config.action_dispatch.x_sendfile_header = nil
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
