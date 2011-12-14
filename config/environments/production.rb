5fe287044814b8167eaa590dcd0e47dc5ec42dcb
Piel de Yeti <pieldeyeti@gmail.com>
2011-12-14 19:42:25 +0100

    Restablecida configuración de producción (heroku): No funciona

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,9 +27,6 @@ FirstApp::Application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
   
-  # Incluido porque en heroku no carga las imagenes de assets
-  config.action_dispatch.x_sendfile_header = nil
-
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
